12d3d4f75274d11d7fd65e515106013aecc68b57
iamvoxlan <novalfourze13@gmail.com>
2021-03-21 15:13:45 +0800

    Deployment setup

--- backend/settings.py
+++ backend/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'invoice_app',
+        'USER': '<db_username>',
+        'PASSWORD': '<password>',
+        'HOST': '<db_host>',
+        'PORT': '<db_port>',
     }
 }
 
